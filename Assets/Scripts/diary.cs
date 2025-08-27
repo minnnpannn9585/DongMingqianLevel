6e1e9837ed20c92f34b2250b795fca281d49bbfc
MingqianDong <196420119+MingqianDong@users.noreply.github.com>
2025-08-26 22:28:22 -0700

    kkk

--- Assets/Scripts/diary.cs
+++ Assets/Scripts/diary.cs
@@ -7,11 +7,41 @@ public class diary : MonoBehaviour
 {
     //public PlayerGrab playerGrab;
     //public Text diaryBoxText;
-    
-    public string diaryText;
-    
 
-  
+    public GameObject diaryPanel;
+    public string diaryText;
+    private bool playerInDiary;
+
+
+    void Update()
+    {
+        if (playerInDiary)
+        {
+            diaryPanel.SetActive(true);
+        }
+        
+
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player") && other.GetType().ToString() == "UnityEngine.CapsuleCollider2D")
+        {
+            Debug.Log("111");
+            playerInDiary = true;
+        }
+    }
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player") && other.GetType().ToString() == "UnityEngine.CapsuleCollider2D")
+        {
+            Debug.Log("222");
+            playerInDiary = false;
+            diaryPanel.SetActive(false);
+        }
+    }
+
+
+
 
 
 
